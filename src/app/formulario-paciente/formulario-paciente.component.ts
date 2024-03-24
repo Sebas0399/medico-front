055fc1ece570d4984cbf74c453f8313d99ed1dea
Sebas0399 <antony.aguay@gmail.com>
2024-03-23 19:05:02 -0500

    fix

--- src/app/formulario-paciente/formulario-paciente.component.ts
+++ src/app/formulario-paciente/formulario-paciente.component.ts
@@ -1,15 +1,83 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Output } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MessageService } from 'primeng/api';
+import { EventEmitter } from '@angular/core';
 import { InputTextModule } from 'primeng/inputtext';
 import { ButtonModule } from 'primeng/button';
+import { Paciente } from '../../domain/paciente';
+import { InputMaskModule } from 'primeng/inputmask';
+
+import { insertarPacienteFachada } from './helpers/savePaciente';
+import { ToastModule } from 'primeng/toast';
+import * as Yup from 'yup';
+import { setLocale } from 'yup';
 
 @Component({
   selector: 'app-formulario-paciente',
   standalone: true,
-  imports: [InputTextModule,ButtonModule],
+  imports: [
+    InputTextModule,
+    ButtonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    ToastModule,
+    InputMaskModule,
+  ],
+  providers: [MessageService],
   templateUrl: './formulario-paciente.component.html',
-  styleUrl: './formulario-paciente.component.css'
+  styleUrl: './formulario-paciente.component.css',
 })
-export class FormularioPacienteComponent {
-  nombre = "Sebas"
-  estaRegistrado = true
+export class FormularioPacienteComponent implements OnInit {
+  @Output() saveSuccess: EventEmitter<any> = new EventEmitter();
+  constructor(private messageService: MessageService) {}
+
+  pacienteIngresar!: Paciente;
+
+  validationSchema: Yup.Schema<Paciente> = Yup.object({
+    cedula: Yup.string()
+      .required('Cédula obligatoria')
+      .min(10, 'Cédula no válida'),
+    fechaNacimiento: Yup.date().required('Fecha de nacimiento obligatoria'),
+  });
+
+  ngOnInit(): void {
+    this.pacienteIngresar = new Paciente();
+  }
+  async guardarPaciente() {
+    await this.validationSchema
+      .validate(this.pacienteIngresar)
+      .then((res) => {
+        insertarPacienteFachada(this.pacienteIngresar)
+          .then((res) => {
+            this.messageService.add({
+              key: 'toastPaciente',
+              severity: 'success',
+              summary: 'OK',
+              detail: 'Paciente insertado correctamente',
+            });
+
+            this.pacienteIngresar = new Paciente();
+            this.saveSuccess.emit(null);
+            //      this.visiblePaciente=false
+          })
+          .catch((err) => {
+            this.messageService.add({
+              key: 'toastPaciente',
+
+              severity: 'error',
+              summary: 'Ocurrio un error',
+              detail: 'Paciente no insertado',
+            });
+          });
+      })
+      .catch((err) => {
+        console.log(err);
+        this.messageService.add({
+          key: 'toastPaciente',
+          severity: 'error',
+          summary: 'OK',
+          detail: err,
+        });
+      });
+  }
 }
--- src/app/tabla-pacientes/tabla-pacientes.component.ts
+++ src/app/tabla-pacientes/tabla-pacientes.component.ts
@@ -10,79 +10,85 @@ import { InputTextModule } from 'primeng/inputtext';
 import { ToastModule } from 'primeng/toast';
 import { CalendarModule } from 'primeng/calendar';
 
+import { FormularioPacienteComponent } from '../formulario-paciente/formulario-paciente.component';
 
-import { obtenerPacienteNombreFachada } from './helpers/getPacientesNombre'
-import { insertarPacienteFachada } from './helpers/savePaciente';
+import { obtenerPacienteNombreFachada } from './helpers/getPacientesNombre';
 import { insertarHistoriaClinicaFachada } from './helpers/saveHistoriaClinica';
 import { Paciente } from '../../domain/paciente';
 import { HistoriaClinica } from '../../domain/historiaClinica';
+import * as Yup from 'yup';
 @Component({
   selector: 'app-tabla-pacientes',
   standalone: true,
-  imports: [TableModule, InputTextModule, FormsModule, ReactiveFormsModule, ButtonModule, DialogModule,ToastModule,CalendarModule],
+  imports: [
+    FormularioPacienteComponent,
+    TableModule,
+    InputTextModule,
+    FormsModule,
+    ReactiveFormsModule,
+    ButtonModule,
+    DialogModule,
+    ToastModule,
+    CalendarModule,
+  ],
   providers: [MessageService],
 
   templateUrl: './tabla-pacientes.component.html',
-  styleUrl: './tabla-pacientes.component.css'
+  styleUrl: './tabla-pacientes.component.css',
 })
 export class TablaPacientesComponent implements OnInit {
-  constructor(private messageService: MessageService) { }
-  pacientes!: Paciente[]
-  pacienteIngresar!: Paciente
-  actualPaciente!:Paciente
-  historiaClinicaIngresar!:HistoriaClinica
-  nombre!: ""
-  historiasClinicas!: HistoriaClinica[]
-  buscandoPacientes = false
-  visibleHistorias = false
-  visiblePaciente = false
-  visibleNuevaHistoria=false
+  
+ 
+  constructor(private messageService: MessageService) {}
+  pacientes!: Paciente[];
+  actualPaciente!: Paciente;
+  historiaClinicaIngresar!: HistoriaClinica;
+  nombre!: '';
+  historiasClinicas!: HistoriaClinica[];
+  buscandoPacientes = false;
+  visibleHistorias = false;
+  visiblePaciente = false;
+  visibleNuevaHistoria = false;
+
   ngOnInit(): void {
-    console.log(this.pacienteIngresar)
-    this.pacienteIngresar = new Paciente()
-    this.actualPaciente=new Paciente
-    this.historiaClinicaIngresar=new HistoriaClinica()
+    this.actualPaciente = new Paciente();
+    this.historiaClinicaIngresar = new HistoriaClinica();
   }
   consultarPorNombre() {
-    console.log("Consultando")
-    this.buscandoPacientes = true
+    console.log('Consultando');
+    this.buscandoPacientes = true;
     obtenerPacienteNombreFachada(this.nombre).then((res) => {
       this.pacientes = res;
-      console.log(res, this.nombre)
-    })
+      console.log(res, this.nombre);
+    });
 
-    this.buscandoPacientes = false
+    this.buscandoPacientes = false;
   }
   abirHistoriaClinica(data: Paciente) {
-    this.actualPaciente=data
-    this.historiasClinicas = data.historiasClinicas as HistoriaClinica[]
-    this.visibleHistorias = true
+    this.actualPaciente = data;
+    this.historiasClinicas = data.historiasClinicas as HistoriaClinica[];
+    this.visibleHistorias = true;
   }
-  guardarPaciente() {
-    insertarPacienteFachada(this.pacienteIngresar).then((res) => {
-      console.log(res)
-      this.messageService.add({
-        severity: 'success', summary: 'OK', detail: 'Paciente insertado correctamente'
-      })
-      this.pacienteIngresar=new Paciente()
-      this.visiblePaciente=false
-
-    }).catch((err)=>{
-      this.messageService.add({
-        severity: 'error', summary: 'Ocurrio un error', detail: 'Paciente no insertado'
-      })
-    })
+  pacienteGuardar() {
+    console.log('guardado');
+    this.visiblePaciente = false;
   }
-  guardarHistoriaClinica(){
-    this.historiaClinicaIngresar.paciente=this.actualPaciente
-    insertarHistoriaClinicaFachada(this.historiaClinicaIngresar).then((res)=>{
-      this.messageService.add({
-        severity: 'success', summary: 'OK', detail: 'Historia clinica insertada correctamente'
+  guardarHistoriaClinica() {
+    this.historiaClinicaIngresar.paciente = this.actualPaciente;
+    insertarHistoriaClinicaFachada(this.historiaClinicaIngresar)
+      .then((res) => {
+        this.messageService.add({
+          severity: 'success',
+          summary: 'OK',
+          detail: 'Historia clinica insertada correctamente',
+        });
       })
-    }).catch((err)=>{
-      this.messageService.add({
-        severity: 'error', summary: 'Ocurrio un error', detail: 'Historia clinica no insertada'
-      })
-    })
+      .catch((err) => {
+        this.messageService.add({
+          severity: 'error',
+          summary: 'Ocurrio un error',
+          detail: 'Historia clinica no insertada',
+        });
+      });
   }
 }
